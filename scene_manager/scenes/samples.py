3da9bea73590ff0461eeaf7679600b980ee65661
Bloodielie <riopro2812@gmail.com>
2020-06-22 13:44:45 +0300

    added filter manager

--- scene_manager/scenes/samples.py
+++ scene_manager/scenes/samples.py
@@ -6,6 +6,8 @@ from scene_manager.scenes.base import BaseScene
 
 
 class MessageScene(BaseScene):
+    _type_scene = "message_scene"
+
     class Config:
         content_types: Optional[Union[ContentType, str]] = [ContentType.ANY]
 
--- scene_manager/tools/filters.py
+++ scene_manager/tools/filters.py
@@ -5,6 +5,7 @@ from aiogram import types
 from aiogram.types import ContentType
 
 from scene_manager.utils import content_type_checker
+from abc import ABC, abstractmethod
 
 
 def context_types_filter(
@@ -33,6 +34,32 @@ def query_data_filter(data: List[str], otherwise_handler: Optional[Callable] = N
                 return await func(self, query)
             elif otherwise_handler is not None:
                 return await otherwise_handler(self, query)
+
+        return wrapper
+
+    return decorator
+
+
+class BaseFilter(ABC):
+    @abstractmethod
+    def __call__(self) -> bool:
+        raise NotImplementedError
+
+    @abstractmethod
+    def otherwise_handler(self):
+        raise NotImplementedError
+
+
+def filter_manager(filter) -> Callable:
+    def decorator(func: Callable):
+        @wraps(func)
+        async def wrapper(self, ctx) -> Callable:
+            filter_obj = filter(ctx)
+            filter_result = await filter_obj()
+            if filter_result:
+                return await func(self, ctx)
+            elif getattr(filter_obj, "otherwise_handler", None) is not None:
+                return await filter_obj.otherwise_handler()
 
         return wrapper
 
